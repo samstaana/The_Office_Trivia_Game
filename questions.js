cecd25e7ff656e194b6dccc4559989f79fbd3185
KS Durazo <sdurazo.UNLV@gmail.com>
2022-03-24 20:53:33 -0700

    added first ten questions

--- questions.js
+++ questions.js
@@ -24,59 +24,59 @@ let questions = [
     correctAnswer: "B"
   },
   {
-    text: `The New York Times slogan is, 'All the News That's Fit to...';
-      A. Digest
-      B. Look
-      C. Read
-      D. Print`,
+    text: `Michael and Dwight tried to steal clients from which local competing business?
+      A. Staples
+      B. Catalyst Paper
+      C. Pendelton
+      D. Prince Family Paper`,
     correctAnswer: "D"
   },
   {
-    text: `Which restaurant's mascot is a clown?
-      A. Chick-Fil-A
-      B. Sonic
-      C. McDonald's
-      D. Whataburger`,
+    text: `Finish Dwight’s security code: “The tea in Nepal is very hot…”
+      A. But the coffee in Cuba is much hotter.
+      B. But the coffee in Cebu is much hotter.
+      C. But the coffee in Peru is much hotter.
+      D. But the coffee in Brazil is much hotter.`,
     correctAnswer: "C"
   },
   {
-    text: `Which of the following is the IATA code for Manchester Airport?
-      A. MAN
-      B. EGLL
-      C. LHR
-      D. EGCC`,
+    text: `Which Harry Potter book did Dwight say he’d take to a desert island?
+      A. Harry Potter and the Prisoner of Azkaban
+      B. Harry Potter and the Order of the Phoenix
+      C. Harry Potter and the Half-Blood Prince
+      D. Harry Potter and the Sorcerer's Stone`,
     correctAnswer: "A"
   },
   {
-    text: `If you are caught 'Goldbricking', what are you doing wrong?
-      A. Stealing
-      B. Cheating
-      C. Slacking
-      D. Smoking`,
+    text: `While playing who would you do in the office, Michael says he’d have sex with which coworker?
+      A. Pam
+      B. Angela
+      C. Ryan
+      D. Danny`,
     correctAnswer: "C"
   },
   {
-    text: `Earth is located in which galaxy?
-      A. The Black Hole
-      B. The Milky Way Galaxy
-      C. The Mars Galaxy
-      D. The Galaxy Note`,
-    correctAnswer: "B"
+    text: `What is Dwight’s all-time favorite movie?
+      A. Star Wars: The Empire Strikes Back
+      B. Lord of the Rings: Return of the King
+      C. The Crow
+      D. Harry Potter and the Prisoner of Azkaban`,
+    correctAnswer: "C"
   },
   {
-    text: `What are Panama hats made out of?
-      A. Silk
-      B. Hemp
-      C. Flax
-      D. Straw`,
+    text: `What does Dwight say is the scariest animal?
+      A. Black Bear
+      B. Lions
+      C. Sharks
+      D. Box Jellyfish`,
     correctAnswer: "D"
   },
   {
-    text: `What kind of aircraft was developed by Igor Sikorsky in the United States in 1942?
-      A. Jet
-      B. Space Capsule
-      C. Helicopter
-      D. Stealth Blimp`,
-    correctAnswer: "C"
+    text: `Why is Michael afraid to move to Colorado?
+    A. He's going to miss his Dunder Mifflin family.
+    B. The television channels will be in the wrong order.
+    C. He'll never see Ryan again.
+    D. He doesn't know anyone is Colorado`,
+    correctAnswer: "B"
   }
 ]
