18a99a0dd9164ef4236926ed38c4d362ffb0193b
KS Durazo <sdurazo.UNLV@gmail.com>
2022-03-24 20:20:53 -0700

    fixed questionaire code

--- questions.js
+++ questions.js
@@ -1,84 +1,82 @@
-//Question template
-
 let questions = [
-    {
-      text: ` Where does Michael Scott move to start his new life with Holly?
-        A. Boston, Massachusetts
-        B. Salt Lake City, Utah
-        C. Boulder, Colorado
-        D. Hope,Idaho',`
-      correctAnswer: "C"
-    },
-    {
-      text: `How would one say goodbye in Spanish?
-        A. Hola
-        B. Au Revoir
-        C. Salir
-        D. Adiós`,
-      correctAnswer: "D"
-    },
-    {
-      text: `What is the French word for 'hat'?
-        A.  Casque
-        B. Chapeau
-        C. Bonnet
-        D. Écharpe`,
-      correctAnswer: "B"
-    },
-    {
-      text: `The New York Times slogan is, 'All the News That's Fit to...';
-        A. Digest
-        B. Look
-        C. Read
-        D. Print`,
-      correctAnswer: "D"
-    },
-    {
-      text: `Which restaurant's mascot is a clown?
-        A. Chick-Fil-A
-        B. Sonic
-        C. McDonald's
-        D. Whataburger`,
-      correctAnswer: "C"
-    },
-    {
-      text: `Which of the following is the IATA code for Manchester Airport?
-        A. MAN
-        B. EGLL
-        C. LHR
-        D. EGCC`,
-      correctAnswer: "A"
-    },
-    {
-      text: `If you are caught 'Goldbricking', what are you doing wrong?
-        A. Stealing
-        B. Cheating
-        C. Slacking
-        D. Smoking`,
-      correctAnswer: "C"
-    },
-    {
-      text: `Earth is located in which galaxy?
-        A. The Black Hole
-        B. The Milky Way Galaxy
-        C. The Mars Galaxy
-        D. The Galaxy Note`,
-      correctAnswer: "B"
-    },
-    {
-      text: `What are Panama hats made out of?
-        A. Silk
-        B. Hemp
-        C. Flax
-        D. Straw`,
-      correctAnswer: "D"
-    },
-    {
-      text: `What kind of aircraft was developed by Igor Sikorsky in the United States in 1942?
-        A. Jet
-        B. Space Capsule
-        C. Helicopter
-        D. Stealth Blimp`,
-      correctAnswer: "C"
-    }
-  ]
+  {
+    text: `Where does Michael Scott move to start his new life with Holly?
+      A. New York, New York
+      B. Akron, Ohio
+      C. Boulder, Colorado
+      D. Salt Lake City, Utah`,
+    correctAnswer: "C"
+  },
+  {
+    text: `How would one say goodbye in Spanish?
+      A. Hola
+      B. Au Revoir
+      C. Salir
+      D. Adiós`,
+    correctAnswer: "D"
+  },
+  {
+    text: `What is the French word for 'hat'?
+      A.  Casque
+      B. Chapeau
+      C. Bonnet
+      D. Écharpe`,
+    correctAnswer: "B"
+  },
+  {
+    text: `The New York Times slogan is, 'All the News That's Fit to...';
+      A. Digest
+      B. Look
+      C. Read
+      D. Print`,
+    correctAnswer: "D"
+  },
+  {
+    text: `Which restaurant's mascot is a clown?
+      A. Chick-Fil-A
+      B. Sonic
+      C. McDonald's
+      D. Whataburger`,
+    correctAnswer: "C"
+  },
+  {
+    text: `Which of the following is the IATA code for Manchester Airport?
+      A. MAN
+      B. EGLL
+      C. LHR
+      D. EGCC`,
+    correctAnswer: "A"
+  },
+  {
+    text: `If you are caught 'Goldbricking', what are you doing wrong?
+      A. Stealing
+      B. Cheating
+      C. Slacking
+      D. Smoking`,
+    correctAnswer: "C"
+  },
+  {
+    text: `Earth is located in which galaxy?
+      A. The Black Hole
+      B. The Milky Way Galaxy
+      C. The Mars Galaxy
+      D. The Galaxy Note`,
+    correctAnswer: "B"
+  },
+  {
+    text: `What are Panama hats made out of?
+      A. Silk
+      B. Hemp
+      C. Flax
+      D. Straw`,
+    correctAnswer: "D"
+  },
+  {
+    text: `What kind of aircraft was developed by Igor Sikorsky in the United States in 1942?
+      A. Jet
+      B. Space Capsule
+      C. Helicopter
+      D. Stealth Blimp`,
+    correctAnswer: "C"
+  }
+]
